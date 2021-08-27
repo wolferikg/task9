4a03edccb6f11267820adba39d35af4810a3ce0d
Wolf Gaudchau <wolf@consol.de>
2021-08-26 22:07:39 -0700

    second commit

---
+++ index.php
@@ -0,0 +1,13 @@
+<html> 
+	<head> 
+	<title>PHP Test</title>
+	<meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1"/>
+	</head>
+
+<body> 
+	<h1>PHP Test</h1>
+		<p><b>Hello World!!!</b></p>
+		<?php echo "The Current Date and Time is: <br />"; 
+		echo date("g:i A l, F j Y.");?> </p>
+	</body> 
+</html>
